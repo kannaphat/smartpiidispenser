a0c24b4fd523e536f4d3a98690c3e6efb93a90e9
kannaphat <kannaphat.mo@mail.kmutt.ac.th>
2017-02-09 17:34:57 +0700

    display main and connect login to secondmain add BaseActivity

--- app/src/main/java/com/example/kannaphat/smartpiidispenser/MainActivity.java
+++ app/src/main/java/com/example/kannaphat/smartpiidispenser/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.kannaphat.smartpiidispenser;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,22 +22,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         EditText text_user = (EditText) findViewById(R.id.text_user);
         EditText text_pass = (EditText) findViewById(R.id.text_pass);
         Button btn_login = (Button) findViewById(R.id.btn_login);
 
         mAuth = FirebaseAuth.getInstance();
+        //รอรับข้อมูลจากuser
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        // User is signed in
+                        Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                    } else {
+                        Log.d("TAG", "onAuthStateChanged:signed_out");
+                    }
+                    // ...
+                }
+            };
+            //press login to secode main
+            btn_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
 
+        @Override
+        public void onStart() {
+            super.onStart();
+            mAuth.addAuthStateListener(mAuthListener);
+        }
 
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(i);
+        @Override
+        public void onStop() {
+            super.onStop();
+            if (mAuthListener != null) {
+                mAuth.removeAuthStateListener(mAuthListener);
             }
-        });
-    }
+        }
 }
 
 
