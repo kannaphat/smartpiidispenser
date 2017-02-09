3d2aeedc02e71314df111681908ac8c4c5424f63
kannaphat <kannaphat.mo@mail.kmutt.ac.th>
2017-02-09 14:19:46 +0700

    display main and connect login to secondmain

--- app/src/main/java/com/example/kannaphat/smartpiidispenser/MainActivity.java
+++ app/src/main/java/com/example/kannaphat/smartpiidispenser/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.kannaphat.smartpiidispenser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText text_user = (EditText) findViewById(R.id.text_user);
+        EditText text_pass = (EditText) findViewById(R.id.text_pass);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
+
+
